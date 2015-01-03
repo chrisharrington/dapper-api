14c386728a8d64732e919c8d3e9b4cfd8ffcb5b7
Chris Harrington <chrisharrington99@gmail.com>
2015-01-03 16:40:24 -0700

    Updated server listening text.

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var server = app.listen(8081, function () {
     var host = server.address().address
     var port = server.address().port
 
-    console.log('Example app listening at http://%s:%s', host, port)
+    console.log("Dapper service listening at http://%s:%s", host, port)
 });
 
 var allowCrossDomain = function(req, res, next) {
