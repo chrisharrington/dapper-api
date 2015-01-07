289bf6d73cbd135a90f08a65585e2e1e0e78b1ad
Chris Harrington <chrisharrington99@gmail.com>
2015-01-06 21:55:01 -0700

    Excluded replies from the tweet list.

--- twitter/recent/index.js
+++ twitter/recent/index.js
@@ -5,7 +5,7 @@ module.exports = function(count) {
     count = count || 10;
         
     return new Promise(function(resolve, reject) {
-        this.get("statuses/user_timeline", function(err, data) {
+        this.get("statuses/user_timeline", { exclude_replies: true, trim_user: true }, function(err, data) {
             if (err) reject(err);
             else resolve(steps(data));
         });
