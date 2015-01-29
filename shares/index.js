0330615e1db15e554d29509f69bad6228ab303c4
Chris Harrington <chrisharrington99@gmail.com>
2015-01-29 16:30:36 -0700

    Added caching for share counts.

--- shares/index.js
+++ shares/index.js
@@ -1,7 +1,11 @@
 var Promise = require("bluebird"),
 	get = require("./get-count"),
 	config = require("../config"),
-	util = require("util");
+	util = require("util"),
+	cache = require("../cache"),
+	moment = require("moment"),
+	
+	CACHE_KEY = "share-counts";
 
 module.exports = function(url) {
 	if (!url) {
@@ -13,17 +17,23 @@ module.exports = function(url) {
 		});
 	}
 	
+	var cached = cache.get(CACHE_KEY + url);
+	if (cached)
+		return Promise.resolve(cached);
+	
 	return Promise.all([
 		require("./twitter")(url),
 		require("./facebook")(url),
 		require("./linkedin")(url),
 		require("./google-plus")(url)
 	]).then(function(result) {
-		return {
+		result = {
 			twitter: result[0],
 			facebook: result[1],
 			linkedin: result[2],
 			googleplus: result[3]
 		};
+		cache.set(CACHE_KEY + url, result, moment().add(1, "h"));
+		return result;
 	});
 };
