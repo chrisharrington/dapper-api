697e4277f3f84bd4a037f686229786d9b549e5cb
Chris Harrington <chrisharrington99@gmail.com>
2015-01-29 16:09:01 -0700

    Added pushed date as secondary search for GitHub.

--- github/sorter.js
+++ github/sorter.js
@@ -1,6 +1,8 @@
 module.exports = {
 	repos: function(repos) {
 		return repos.sort(function(first, second) {
+			if (first.starCount === second.starCount)
+				return first.date > second.date ? -1 : 1;
 			return first.starCount > second.starCount ? -1 : 1;
 		});
 	}
